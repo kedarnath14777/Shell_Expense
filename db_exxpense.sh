f623fbfe54c7ed863bb1b33770bbbf4d435351f5
kedarnath3699 <kedarnath.a3699@gamil.com>
2024-07-24 21:15:49 +0530

    db file

--- db_exxpense.sh
+++ db_exxpense.sh
@@ -5,7 +5,14 @@ USER_ID=$(id -u )
 
 #create the timestamp and log file 
 TIME_STAMP=$(date +%F-%H-%M-%S )
-echo  $TIME_STAMP
 SRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LL=/tmp/$SRIPT_NAME-$TIME_STAMP.log
+#check user is sudo or not 
+if [ $USER_ID -eq 0 ]
+ echo "super user"
+else
+ echo "need super user sreadentails"
+ exit 1 # exit manually 
+fi 
 
-echo $SRIPT_NAME
+echo "wellcome"
