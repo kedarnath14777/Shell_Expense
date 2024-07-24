20e33c3a2f4cc28488f59f8d62cbbf162783764e
kedarnath3699 <kedarnath.a3699@gamil.com>
2024-07-24 21:32:21 +0530

    db file

--- db_exxpense.sh
+++ db_exxpense.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-
-
 USER_ID=$(id -u )
 
 #create the timestamp and log file 
 TIME_STAMP=$(date +%F-%H-%M-%S )
 SRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LL=/tmp/$SRIPT_NAME-$TIME_STAMP.log
+R=\e[31m
+G=\e[32m
+Y=\e[33m
+N=\e[0+m
 #check user is sudo or not 
 if [ $USER_ID -eq 0 ]
 then 
@@ -16,4 +18,15 @@ else
  exit 1 # exit manually 
 fi 
 
-echo "wellcome"
+exit_status(){
+    if [ $1 -eq 0 ]
+    then 
+     echo "installed successfully.. $2"
+    else
+    echo "not installed.. $2"
+    exit 1 # exit manually 
+}
+
+dnf install mysql-server -y
+exit_status $? "mysql server"
+
