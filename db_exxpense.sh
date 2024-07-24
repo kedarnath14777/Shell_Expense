419de1ac0ad09272eba8248f078f1c1364482543
kedarnath3699 <kedarnath.a3699@gamil.com>
2024-07-24 21:10:19 +0530

    db file

--- db_exxpense.sh
+++ db_exxpense.sh
@@ -6,6 +6,6 @@ USER_ID=$(id -u )
 #create the timestamp and log file 
 TIME_STAMP=$(date +%F-%H-%M-%S )
 echo  $TIME_STAMP
-SRIPT_NAME=$( $0 | cut -d "." -f1 )
+SRIPT_NAME=$(( $0 | cut -d "." -f1 ))
 
 echo $SRIPT_NAME
