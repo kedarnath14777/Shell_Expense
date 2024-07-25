9f6be12dac255795ab1b71d1a5e1d1222041b38a
kedarnath3699 <kedarnath.a3699@gamil.com>
2024-07-25 21:25:20 +0530

    2

--- 05-conf.sh
+++ 05-conf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
 read -p "pelaes enter the username:"  USERNAME
-read -p  -s " please enter the Password ::" PASS 
+read -s " please enter the Password ::" PASS 
 
 echo "user is $USERNAME and Pass is $PASS"
