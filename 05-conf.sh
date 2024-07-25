ddb7f6f1e01ae437f5dc89b289fad6d4874538f6
kedarnath3699 <kedarnath.a3699@gamil.com>
2024-07-25 21:29:17 +0530

    2

--- 05-conf.sh
+++ 05-conf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-read -p "pelaes enter the username:"  USERNAME
-read -s " please enter the Password ::" PASS 
+read -p "pelaes enter the username: : "  USERNAME
+read -p " please enter the Password ::"  PASS 
 
 echo "user is $USERNAME and Pass is $PASS"
---
+++ 06-datatypes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+echo "sum of two numbers"
+
+num1=$1
+num2=$2
+
+sum=$((num1+num2))
+echo "sum of two numbers" $sum
