70a1e508d79828d06cda73222d3700bd1bc49dbf
kedarnath3699 <kedarnath.a3699@gamil.com>
2024-07-25 21:14:12 +0530

    2

---
+++ 03-Var_prompt.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+person1=$1
+person2=$2
+
+echo "$person1 :: Hi $person2 "
+echo "$person2 :: How are you "
+echo "$person1 :: good how are you $person2" 
+echo "$person2 :: me good what about you $person1"
+echo "$person1:: upgading to Other Tools $person2 "
