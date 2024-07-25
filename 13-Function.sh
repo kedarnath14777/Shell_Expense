72e6a61ef47cd3f5a4abccb5a174a7a8dc549183
kedarnath3699 <kedarnath.a3699@gamil.com>
2024-07-26 00:16:31 +0530

    2

--- 13-Function.sh
+++ 13-Function.sh
@@ -2,17 +2,16 @@
 
 UU=$(id -u )
 
-valid_package(){
-    echo "exist status is :: $1"
-    echo "whats is :: $2"
-    if [ $1 -eq 0 ]
-     then 
-        echo " installed success fully::$2"
-     else
-        echo " not installed ::$2" 
-        exit 1 # manually exxiting 
+valid_package(){ 
+     if [ $1 -eq 0 ]
+    then
+        echo "installed succeess $2"
+    else 
+        echo "not installed $2"
+        exit 1 # manually 
     fi 
- }
+
+}
 
 if [ $UU -eq 0 ]
 then 
