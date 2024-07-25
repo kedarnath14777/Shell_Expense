bce6621abf45b1af8b55b358e35512cf1044d962
kedarnath3699 <kedarnath.a3699@gamil.com>
2024-07-25 22:37:32 +0530

    2

--- 10-Array.sh
+++ 10-Array.sh
@@ -3,7 +3,6 @@
 courses=("Ansible" "git" "docker" "Kubernetes")
 
 echo "1st course is ${courses[0]}"
-
-echo "all courses are ${courses[*]}
-
-
+echo "1st course is ${courses[1]}"
+echo "1st course is ${courses[2]}"
+echo "1st course is ${courses[@]}"
