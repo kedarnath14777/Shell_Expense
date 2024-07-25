0da89d95b5ec7967188d0dca9185232c1fabdc3c
kedarnath3699 <kedarnath.a3699@gamil.com>
2024-07-25 23:48:42 +0530

    2

--- 13-Function.sh
+++ 13-Function.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 UU=$(id -u )
+
+valid_package(){
+    if [ $1 -eq 0 ]
+    then 
+         echo " installed successfully $2"
+    else
+        echo " not installed $2"
+        exit 1 # manually exit 
+
+}
 if [ $UU -eq 0 ]
 then 
     echo "super user"
@@ -9,3 +19,5 @@ else
     exit 1 # exit manually 
 fi 
 
+dnf install nginx -y 
+valid_package $? "nginx"
