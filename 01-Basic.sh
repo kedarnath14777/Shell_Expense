6f5483984416e40f68a9afcace831d26ea84682b
kedarnath3699 <kedarnath.a3699@gamil.com>
2024-07-25 21:07:07 +0530

    2

---
+++ 01-Basic.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+echo "hello welcome"
+
+echo " con between two persons"
+
+
+echo "Ramesh :: Hi Rohan "
+echo "Rohan :: How are you "
+echo "Ramesh :: good how are you" 
+echo "Rohan :: me good what about you"
+echo "rohan:: upgading to Other Tools"
