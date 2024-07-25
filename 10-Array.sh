644a8f93850512f816ed1840c5757a59d53dcfe4
kedarnath3699 <kedarnath.a3699@gamil.com>
2024-07-25 22:18:08 +0530

    2

--- 10-Array.sh
+++ 10-Array.sh
@@ -2,17 +2,7 @@
 
 Array1=("Ansible","git","docker","Kubernetes")
 
-echo "print course 1 is ::"$Array1[$0]
 
-echo "print course 1 is ::" $Array1[$1]
-
-echo "print course 1 is ::"$Array1[$2]
-
-echo "print course 1 is ::"$Array1[$@]
+echo "print course 1 is ::"Array1[$@]
 
 
-
-for i in $Array1
-do 
-  echo "couses are ::" $[i]
-done 
