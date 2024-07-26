c2ad67d8c1bba04601701f9e9959d3da62940b4b
kedarnath3699 <kedarnath.a3699@gamil.com>
2024-07-26 20:00:39 +0530

    4

--- mysql.sh
+++ mysql.sh
@@ -45,7 +45,7 @@ valid $? "start the service"
 # mysql_secure_installation --set-root-pass ExpenseApp@1   &>> $l_file
 # valid $? "db password setuped"
 echo "hello 1"
-mysql -h -uroot -p ${PASSWD} -e "show databases"   &>> $l_file
+mysql -h -uroot -p $PASSWD -e "show databases"   &>> $l_file
 if [ $? -eq 0 ]
 then 
     echo "hello 2"
@@ -53,7 +53,7 @@ then
 else
     echo "hello 3"
     echo "need to setup the password" 
-     mysql_secure_installation --set-root-pass ${PASSWD}  &>> $l_file
+     mysql_secure_installation --set-root-pass $PASSWD  &>> $l_file
      valid $? "root password setuped "
      echo "hello 4"
 fi 
